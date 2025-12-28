833d62443d951b0bbdc89a2d0d61306ce214d16a
alamantik <alamantik@gmail.com>
2025-12-29 01:31:23 +0200

    searching with rank

--- FurnitureStoreApp/goods/utils.py
+++ FurnitureStoreApp/goods/utils.py
@@ -5,15 +5,7 @@ def search_q(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     
-    vector = SearchVector('name', 'description')
-    query = SearchQuery(query)
+    vector = SearchVector('name', 'description', config='english')
+    query = SearchQuery(query, config='english')
 
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
-
-    # keywords = [word for word in query.split() if len(word) > 3]
-    # q_objects = Q()
-    # for token in keywords:
-    #     q_objects |= Q(description__icontains = token)
-    #     q_objects |= Q(name__icontains = token)
-
-    # return Products.objects.filter(q_objects)
+    return Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0.00001).order_by("-rank")
