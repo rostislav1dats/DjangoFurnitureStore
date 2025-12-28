475ece3b64796bc1584895adcb816c63c1c4feb4
alamantik <alamantik@gmail.com>
2025-12-29 01:00:41 +0200

    full text search

--- FurnitureStoreApp/goods/utils.py
+++ FurnitureStoreApp/goods/utils.py
@@ -1,14 +1,19 @@
-from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .models import Products
 
 def search_q(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     
-    keywords = [word for word in query.split() if len(word) > 3]
-    q_objects = Q()
-    for token in keywords:
-        q_objects |= Q(description__icontains = token)
-        q_objects |= Q(name__icontains = token)
+    vector = SearchVector('name', 'description')
+    query = SearchQuery(query)
 
-    return Products.objects.filter(q_objects)
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+
+    # keywords = [word for word in query.split() if len(word) > 3]
+    # q_objects = Q()
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains = token)
+    #     q_objects |= Q(name__icontains = token)
+
+    # return Products.objects.filter(q_objects)
